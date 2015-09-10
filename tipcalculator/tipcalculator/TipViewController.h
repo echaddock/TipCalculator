3d226ef12e53d6f0e796c03b7044fe5f7dda0ce5
Liz Chaddock <lizchaddock@bloodjourney-lm.corp.yahoo.com>
2015-09-10 15:21:46 -0700

    Initial tip app from video tutorial

---
+++ tipcalculator/tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Liz Chaddock on 9/10/15.
+//  Copyright (c) 2015 Liz Chaddock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
